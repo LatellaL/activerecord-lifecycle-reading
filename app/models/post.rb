ae3d28f3cc28b95b12dce6b3387f107c39ca483a
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-04 20:48:54 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,30 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  # before_save :make_title_case
+  before_validation :make_title_case
+
+  # Here is a rule of thumb: Whenever you are modifying an attribute of 
+  # the model, use before_validation. 
+  # If you are doing some other action, then use before_save.
 
   private
 
+
+    
+          
+            
+    
+
+          
+    
+    
+  
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
     end
   end
-
   def make_title_case
     self.title = self.title.titlecase
   end
